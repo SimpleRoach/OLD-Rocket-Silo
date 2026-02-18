c8eefcb3f804909ccafe20fbf1a4a79235f05c8a
SimpleRoach <tlsmscience@gmail.com>
2026-02-18 22:25:59 +0300

    data.lua init

---
+++ data.lua
@@ -0,0 +1,7 @@
+require("prototypes.entity")
+require("prototypes.item")
+require("prototypes.recipe")
+
+-- скрываем ванильную шахту
+data.raw["rocket-silo"]["rocket-silo"].hidden = true
+data.raw.recipe["rocket-silo"].hidden = true
